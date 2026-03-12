00e07a9cab9de6db013e4e20d4d057244f97cab1
Adam Clark <adamclark@Mac.home>
2026-03-12 11:12:15 +0100

    Test minimal API

--- api/send-report.js
+++ api/send-report.js
@@ -1,3 +1,7 @@
+import { Resend } from "resend";
+
 export default async function handler(req, res) {
-  return res.status(200).json({ ok: true });
+  return res.status(200).json({
+    hasKey: !!process.env.RESEND_API_KEY
+  });
 }
