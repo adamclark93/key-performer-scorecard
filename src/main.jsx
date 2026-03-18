f202878e84859054edf2f01e731420f6d9c91369
Adam Clark <adamclark@Mac.home>
2026-03-18 15:18:29 +0100

    add privacy policy route

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,16 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Scorecard from './components/Scorecard.jsx'
+import PrivacyPolicy from './components/PrivacyPolicy.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Scorecard />} />
+        <Route path="/privacy" element={<PrivacyPolicy />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>
-);
+)
